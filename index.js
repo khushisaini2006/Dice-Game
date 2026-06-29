be70417ae00d9fc89944616e5a04150cb21f2e4c
khushisaini2006 <khushi2006saini@gmail.com>
2026-06-29 18:43:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -19,10 +18,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
